3de16129d4bea5711f45c02e3dd8023f82b1286f
J. Peter M. Schuler <j.peter.m.schuler@uni-due.de>
2024-10-10 15:47:19 +0200

    [BUGFIX] pages type icon definition should be in TCA folder

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -3,3 +3,6 @@
 defined('TYPO3') or die();
 // Register typeicon (the icon itself is registered in ext_tables.php)
 $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-news'] = 'tcarecords-pages-contains-news';
+// add folder icon
+$GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-tt_news'] = 'apps-pagetree-folder-contains-news';
+$GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [0 => 'LLL:EXT:tt_news/Resources/Private/Language/locallang_tca.xlf:tt_news', 1 => 'tt_news', 2 => 'apps-pagetree-folder-contains-news'];
--- ext_tables.php
+++ ext_tables.php
@@ -99,10 +99,6 @@ $boot = function () {
             BitmapIconProvider::class,
             ['source' => 'EXT:tt_news/Resources/Public/Images/Icons/tt_news_exturl.gif']
         );
-
-        // add folder icon
-        $GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-tt_news'] = 'apps-pagetree-folder-contains-news';
-        $GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [0 => 'LLL:EXT:tt_news/Resources/Private/Language/locallang_tca.xlf:tt_news', 1 => 'tt_news', 2 => 'apps-pagetree-folder-contains-news'];
     }
 };
 
